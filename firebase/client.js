94982cd39033173c5e8ba2cdf9b8ed76fd58b0a6
Irving Salmeron <irving.salmeron@misalud.ai>
2022-12-21 10:27:02 -0600

    Use env vars instead of having the data there and remove caret for versions

--- firebase/client.js
+++ firebase/client.js
@@ -11,13 +11,13 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDK1APtOQVTsyv0W6OrWTxCTe0taqelu74",
-  authDomain: "facturaciontest-4b729.firebaseapp.com",
-  projectId: "facturaciontest-4b729",
-  storageBucket: "facturaciontest-4b729.appspot.com",
-  messagingSenderId: "636381183298",
-  appId: "1:636381183298:web:c410a0bba8d514d5b9dcdb",
-  measurementId: "G-RWMKL76RDB"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMEND_ID,
 };
 
 // Initialize Firebase
