c1e91824aee462f97a1ac21b706fb1706d6ed8c7
Irving Salmeron <irving.salmeron@misalud.ai>
2023-01-23 12:37:28 -0600

    Improve style on text inputs for login, update packages

--- components/TextField/TextField.js
+++ components/TextField/TextField.js
@@ -2,18 +2,18 @@ import React from "react";
 import styled from "styled-components";
 
 const TextInput = styled.input.attrs((props) => ({
-  type: props.type || 'text',
+  type: props.type || "text",
 }))`
   margin: 2em;
-  padding: 0.5em;
+  padding: 0.7em;
   border-radius: 16px;
   border: 1px solid #0000003b;
   outline: none;
 
   &.error {
-    box-shadow: 0 0 3px 1px rgba(214,0,0, 0.3);
+    box-shadow: 0 0 3px 1px rgba(214, 0, 0, 0.3);
   }
-`
+`;
 
 export const TextField = React.forwardRef(({ value, onChange, type, error, ...rest }, ref) => (
   <TextInput
--- pages/login.js
+++ pages/login.js
@@ -48,79 +48,79 @@ const FormGroup = styled.div`
 
 const LoginEmail = styled.input.attrs(() => ({
   type: "text",
-  name: 'email',
+  name: "email",
 }))`
   min-width: 20em;
   margin: 2em;
-  padding: 0.5em;
+  padding: 1em;
   border-radius: 16px;
   border: 1px solid #0000003b;
   outline: none;
   transition: all ease 0.2s;
 
   &:hover {
-    box-shadow: 0 10px 10px 1px rgba(0,0,0, 0.2);
+    box-shadow: 0 10px 10px 1px rgba(0, 0, 0, 0.2);
   }
 `;
 
 const LoginPassword = styled.input.attrs(() => ({
   type: "password",
-  name: 'password',
+  name: "password",
 }))`
   min-width: 20em;
   margin: 2em;
-  padding: 0.5em;
+  padding: 1em;
   border-radius: 16px;
   border: 1px solid #0000003b;
   outline: none;
   transition: all ease 0.2s;
 
   &:hover {
-    box-shadow: 0 10px 10px 1px rgba(0,0,0, 0.2);
+    box-shadow: 0 10px 10px 1px rgba(0, 0, 0, 0.2);
   }
 `;
 
 const LoginButton = styled.button`
-  background-color: #1993FF;
-  color: #FFFFFF;
+  background-color: #1993ff;
+  color: #ffffff;
   border-radius: 16px;
   padding: 1em;
-  margin: 1em;
+  margin: 5em;
   border: none;
   cursor: pointer;
   width: 250px;
   transition: all ease 0.2s;
 
   &:hover {
-    background-color: #1166B2;
+    background-color: #1166b2;
   }
 `;
 
 const ErrorMessage = styled.div`
-  color: #CC0000;
+  color: #cc0000;
   font-size: 0.85rem;
   text-align: center;
   font-weight: 500;
-`
+`;
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState();
   const { user, logIn } = useContext(userContext);
   const router = useRouter();
 
   useEffect(() => {
-    if (user?.isAuthenticated) router.replace('/');
-  }, [user])
+    if (user?.isAuthenticated) router.replace("/");
+  }, [user]);
 
   const onSubmit = () => {
-    setError(undefined)
+    setError(undefined);
     logIn(email, password).catch((error) => {
-      console.log("There was an error: ", errorCodeToMessage(error.code))
-      setError(errorCodeToMessage(error.code))
+      console.log("There was an error: ", errorCodeToMessage(error.code));
+      setError(errorCodeToMessage(error.code));
     });
-  }
+  };
 
   return (
     <>
@@ -129,26 +129,34 @@ function Login() {
       </Head>
       <LoginWrapper>
         <LoginContainer>
-          <h1>Inicio</h1>
+          <h1>Iniciar sesión</h1>
           <FormContainer>
             <FormGroup>
               <div>Correo: </div>
               <div>
-                <LoginEmail onChange={(event) => setEmail(event.target.value)} value={email} />
+                <LoginEmail
+                  onChange={(event) => setEmail(event.target.value)}
+                  value={email}
+                />
               </div>
             </FormGroup>
             <FormGroup>
               <div>Contraseña: </div>
-              <LoginPassword onChange={(event) => setPassword(event.target.value)} value={password} />
+              <LoginPassword
+                onChange={(event) => setPassword(event.target.value)}
+                value={password}
+              />
             </FormGroup>
             {error ? <ErrorMessage>{error}</ErrorMessage> : undefined}
           </FormContainer>
           <LoginButton onClick={onSubmit}>Iniciar Sesión</LoginButton>
-          <div>Aún no tienes cuenta? <Link href="/signup">Regístrate aquí</Link></div>
+          <div>
+            Aún no tienes cuenta? <Link href="/signup">Regístrate aquí</Link>
+          </div>
         </LoginContainer>
       </LoginWrapper>
     </>
-  )
+  );
 }
 
 Login.getLayout = function getLayout(page) {
