6745bfecad799b5bc9f3394f1606631f2d610991
isalmeron <irvingsalmeron40@gmail.com>
2023-01-22 22:23:08 -0600

    Add data fixtures

---
+++ data/fixtures/empleado.js
@@ -0,0 +1,20 @@
+export const EMPLEADO = {
+  id: "EMPLEADO-1",
+  nombre: "Irving Salmeron",
+  rfc: "SAPI901210DA8",
+  razonSocial: "Irving Salmeron",
+  regimen: "Asalariado",
+  numeroSeguroSocial: "14253671523",
+  direccion: {
+    calle: "Tecozautla",
+    colonia: "Roma Sur",
+    numero: "123",
+    codigoPostal: "23415",
+    estado: "CDMX",
+    municipio: "Cuahutemoc",
+  },
+  percepciones: {
+    salario: 1000,
+    conceptos: [],
+  },
+};
---
+++ data/fixtures/empresa.js
@@ -0,0 +1,17 @@
+export const EMPRESA = {
+  id: "empresa-1",
+  nombre: "EMPRESA 1",
+  rfc: "EMPRESA120DA",
+  razonSocial: "EMPRESA 1 SA de CV",
+  regimen: "Persona fisica con actividad empresarial",
+  direccion: {
+    calle: "Alvaro obregon",
+    colonia: "Roma Norte",
+    numeroExt: "123",
+    numeroInt: "12A",
+    codigoPostal: "76127",
+    estado: "CDMX",
+    municipio: "Cuahutemoc",
+  },
+  periodo: {},
+};
